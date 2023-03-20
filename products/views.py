bef4281890d7073223628dfe5fedfc8841b32570
Farah M <farah.maria.rahman@gmail.com>
2023-03-20 23:29:47 +0000

    temporarily add yellow as class for text on products page to see experimental changes in views.py and urls.py to get two models on one page - see bugs in readme

--- products/views.py
+++ products/views.py
@@ -2,14 +2,23 @@ from django.shortcuts import render
 from .models import Book, Featured_Product
 
 
-def all_products(request):
+def all_books(request):
     """A view to return the products pages, incl sorting & search queries"""
     books = Book.objects.all()
-    featured_products = Featured_Product.objects.all()
 
     context = {
         'books': books,
-        'featured_products': featured_products,
+    }
+
+    return render(request, 'products/products.html', context)
+
+
+def all_featured(request):
+    """A view to return the products pages, incl sorting & search queries"""
+    featured = Featured_Product.objects.all()
+
+    context = {
+        'featured': featured,
     }
 
     return render(request, 'products/products.html', context)
