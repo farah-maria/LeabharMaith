fd2c978c1d293cc3f30b1acf4e279ae023dd20d8
Farah M <farah.maria.rahman@gmail.com>
2023-03-20 21:59:27 +0000

    add skeleton code for basis of views.py in products app

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Book, Featured_Product
 
-# Create your views here.
+
+def all_products(request):
+    """A view to return the products pages, incl sorting & search queries"""
+    books = Book.objects.all()
+    featured_products = Featured_Product.objects.all()
+
+    context = {
+        'books': books,
+        'featured_products': featured_products,
+    }
+
+    return render(request, 'products/products.html', context)
