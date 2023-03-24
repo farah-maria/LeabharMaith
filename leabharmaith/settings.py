9752c6491088c326c7f219015ca921cab07304d2
Farah M <farah.maria.rahman@gmail.com>
2023-03-24 15:51:49 +0000

    change debug back to to true, and other settings so I can see images in IDE

--- leabharmaith/settings.py
+++ leabharmaith/settings.py
@@ -26,9 +26,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-X_FRAME_OPTIONS = 'SAMEORIGIN'  # for summernotes. delete if not using.
+DEBUG = True
 
 ALLOWED_HOSTS = ['leabhar-maith.herokuapp.com', 'localhost']
 
