bdfa276c053cd1ed36a384ab86459a6f0359d167
Farah M <farah.maria.rahman@gmail.com>
2023-03-29 22:49:52 +0000

    link products page to home page via the show now button

--- products/views.py
+++ products/views.py
@@ -13,3 +13,17 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request):
+    """ A view to show individual product details """
+
+    book = get_object_or_404(Book)
+    featured_product = get_object_or_404(Featured_Product)
+
+    context = {
+        'book': book,
+        'featured_product': featured_product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
