49646db800b18e7d443ddbf75c45e9bb916dc38b
Farah M <farah.maria.rahman@gmail.com>
2023-04-01 00:15:18 +0000

    add url, view and template tags in template for viewing book_detail via clicking on a cover image on products page

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('<int:book_id>/book_detail/', views.book_detail, name='book_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Book, Author, Featured_Product
 
 
@@ -15,15 +19,13 @@ def all_products(request):
     return render(request, 'products/products.html', context)
 
 
-def product_detail(request):
-    """ A view to show individual product details """
+def book_detail(request, book_id):
+    """ A view to show individual book details """
 
-    book = get_object_or_404(Book)
-    featured_product = get_object_or_404(Featured_Product)
+    book = get_object_or_404(Book, pk=book_id)
 
     context = {
         'book': book,
-        'featured_product': featured_product,
     }
 
-    return render(request, 'products/product_detail.html', context)
+    return render(request, 'products/book_detail.html', context)
