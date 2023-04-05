14ee3d1849fa106eca353eba2426e1f1a84415cf
Farah M <farah.maria.rahman@gmail.com>
2023-04-05 10:39:36 +0000

    install whitenoise and add to settings pluis requirements.txt

--- products/views.py
+++ products/views.py
@@ -29,34 +29,10 @@ def all_products(request):
         if 'q' in request.GET:
             query = request.GET['q']
 
-    if request.GET:
-        if 'sort' in request.GET:
-            sortkey = request.GET['sort']
-            sort = sortkey
-            if sortkey == 'name':
-                sortkey = 'lower_name'
-                books = books.annotate(lower_name=Lower('name'))
-            if sortkey == 'category':
-                sortkey = 'category__name'
-            if 'direction' in request.GET:
-                direction = request.GET['direction']
-                if direction == 'desc':
-                    sortkey = f'-{sortkey}'
-            books = books.order_by(sortkey)
-
-        if 'category' in request.GET:
-            categories = request.GET['category'].split(',')
-            books = books.filter(category__name__in=categories)
-            categories = Category.objects.filter(name__in=categories)
-
-    current_sorting = f'{sort}_{direction}'
-
     context = {
         'books': books,
         'featured_products': featured_products,
         'search_term': query,
-        'current_categories': categories,
-        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
