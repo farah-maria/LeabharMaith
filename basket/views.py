b72c3a287918bed4910d368d2588696b57fc5b70
Farah M <farah.maria.rahman@gmail.com>
2023-04-05 16:35:51 +0000

    add logic to basket views.py for 'remove from basket' functionality

--- basket/views.py
+++ basket/views.py
@@ -47,6 +47,20 @@ def adjust_basket(request, book_id):
 
     request.session['basket'] = basket
     return redirect(reverse('view_basket'))
-    request.session['basket'] = basket
-    return redirect(reverse('view_basket'))
 
+
+def remove_from_basket(request, book_id):
+    """Remove book from the shopping bag"""
+    book = get_object_or_404(Book, pk=book_id)
+    try:
+        basket = request.session.get('basket', {})
+        basket.pop(book_id)
+        messages.success(
+            request,
+            f'"{book.title}" has been removed from your basket')
+
+        request.session['basket'] = basket
+        return HttpResponse(status=200)
+
+    except Exception as error:
+        return HttpResponse(content=error, status=500)
