c59cf5e5a7bb2366b57eba3c18509d04f011735a
Farah M <farah.maria.rahman@gmail.com>
2023-04-08 16:35:40 +0000

    styling - change the colour for most  hover effects to green

--- leabharmaith/urls.py
+++ leabharmaith/urls.py
@@ -19,8 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('basket/', include('basket.urls')),
--- products/urls.py
+++ products/urls.py
@@ -7,11 +7,4 @@ urlpatterns = [
           name='book_detail'),
      path('<int:featured_id>/product_detail/', views.featured_detail, 
           name='featured_detail'),
-     path('admin/', admin.site.urls),
-     path('accounts/', include('allauth.urls')),
-     path('', include('home.urls')),
-     path('products/', include('products.urls')),
-     path('basket/', include('basket.urls')),
-     path('checkout/', include('checkout.urls')),
-     path('profile/', include('profiles.urls')),
 ]
