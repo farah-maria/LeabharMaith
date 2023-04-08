e20a7aee16862d16369a7278f4e1c18597b06bc1
Farah M <farah.maria.rahman@gmail.com>
2023-04-08 11:42:06 +0000

    add urls for basket etc to project level urls.py file

--- products/urls.py
+++ products/urls.py
@@ -2,9 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_products, name='products'),
-    path('<int:book_id>/product_detail/', views.book_detail,
-         name='book_detail'),
-    path('<int:featured_id>/product_detail/', views.featured_detail,
-         name='featured_detail')
+     path('', views.all_products, name='products'),
+     path('<int:book_id>/product_detail/', views.book_detail, 
+          name='book_detail'),
+     path('<int:featured_id>/product_detail/', views.featured_detail, 
+          name='featured_detail'),
+     path('admin/', admin.site.urls),
+     path('accounts/', include('allauth.urls')),
+     path('', include('home.urls')),
+     path('products/', include('products.urls')),
+     path('basket/', include('basket.urls')),
+     path('checkout/', include('checkout.urls')),
+     path('profile/', include('profiles.urls')),
 ]
