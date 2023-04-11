5d425c10321112c10ec019d2c84088b0429d9a71
Farah M <farah.maria.rahman@gmail.com>
2023-04-11 18:50:07 +0000

    tidy up code in checkout app so far and add url for checkout on project level

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,4 +1,4 @@
-""" source code: Boutique Ado project, Code Institute """
+""" based on Boutique Ado project, Code Institute """
 from django.contrib import admin
 from .models import Order, OrderLineItem
 
