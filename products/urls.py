f7d690960b1aac84797cb1c88462dd1470ec380f
Farah M <farah.maria.rahman@gmail.com>
2023-04-15 12:40:11 +0000

    add product ids to urls for editing products, as these were missing before

--- products/urls.py
+++ products/urls.py
@@ -10,7 +10,9 @@ urlpatterns = [
      path('add_book/', views.add_book, name='add_book'),
      path('add_featured/', views.add_featured, name='add_featured'),
      path('add_author/', views.add_author, name='add_author'),
-     path('edit_book/', views.edit_book, name='edit_book'),
-     path('edit_featured/', views.edit_featured, name='edit_featured'),
-     path('edit_author/', views.edit_author, name='edit_author'),
+     path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
+     path('edit_featured/<int:featured_id>/', views.edit_featured,
+          name='edit_featured'),
+     path('edit_author/<int:author_id>/', views.edit_author,
+          name='edit_author'),
 ]
