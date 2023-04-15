771733db381c1491707e2fa53c1dce2e03dfbb67
Farah M <farah.maria.rahman@gmail.com>
2023-04-15 12:21:10 +0000

    crate urls for edit product pages

--- products/urls.py
+++ products/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
      path('add_book/', views.add_book, name='add_book'),
      path('add_featured/', views.add_featured, name='add_featured'),
      path('add_author/', views.add_author, name='add_author'),
+     path('edit_book/', views.edit_book, name='edit_book'),
+     path('edit_featured/', views.edit_featured, name='edit_featured'),
+     path('edit_author/', views.edit_author, name='edit_author'),
 ]
