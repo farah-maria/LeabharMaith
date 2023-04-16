a0d4b3a2be48234cdb60382e46bda3394a84823a
Farah M <farah.maria.rahman@gmail.com>
2023-04-16 23:05:39 +0000

    add style to newsletter signup form

--- extras/urls.py
+++ extras/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.about, name='about'),
+    
     path('newsletter/', views.newsletter, name='newsletter'),
-    path('contact/', views.contact, name='contact'),
-    path('privacy/', views.privacy, name='privacy'),
+    
 ]
--- home/views.py
+++ home/views.py
@@ -7,9 +7,3 @@ def index(request):
     """A view to return the index page"""
 
     return render(request, 'home/index.html')
-
-
-def newsletter(request):
-    """a view to return the newsletter signup page"""
-
-    return render(request, 'home/newsletter.html')
